bc64b2f0ec60b3047c7cf0563e3cd23ab9a34bd2
maziargomrokchi <mgomrokchi@gmail.com>
2021-05-27 11:11:33 -0400

    BCQ now generates learned trajectories with the same initial states as those generated by DDPG

--- read_npy.py
+++ read_npy.py
@@ -1,3 +1,24 @@
 import numpy as np
 
-print(np.load("./Hopper-v3/BCQ_Hopper-v2_100_700000.npy"))
+a = np.load("/Users/maziargomrokchi/learning_output/Hopper-v3/2000/100/20/buffers/Robust_Hopper-v3_100_initial_state.npy")
+b = np.load("/Users/maziargomrokchi/learning_output/Hopper-v3/2000/100/20/buffers/Robust_Hopper-v3_100_action.npy")
+c = np.load("/Users/maziargomrokchi/learning_output/Hopper-v3/2000/100/20/buffers/Robust_Hopper-v3_100_reward.npy")
+d = np.load("/Users/maziargomrokchi/learning_output/Hopper-v3/2000/100/20/buffers/Robust_Hopper-v3_100_state.npy")
+e = np.load("/Users/maziargomrokchi/learning_output/Hopper-v3/2000/100/20/buffers/Robust_Hopper-v3_100_next_state.npy")
+print('a' , np.shape(a))
+print('b' , np.shape(b))
+print('c', np.shape(c))
+print('d', np.shape(d))
+print('e', np.shape(e))
+# print(a)
+# print(d)
+
+
+print(np.load("/Users/maziargomrokchi/learning_output/Hopper-v3/500/1/15/results/behavioral_Hopper-v3_1.npy"))
+
+
+f = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+g = np.array([0, 2])
+print(np.shape(g))
+h = ([f[g[i], :] for i in range(len(g))])
+print(len(h))
--- runner_v2.py
+++ runner_v2.py
@@ -130,7 +130,8 @@ def train_BCQ(attack_path, state_dim, action_dim, max_action, device, args):
 # A fixed seed is used for the eval environment
 def eval_policy(policy, env_name, seed, eval_episodes=10, max_episode_step=None):
     eval_env = gym.make(env_name)
-    eval_env.seed(seed + 100)
+    # eval_env.seed(seed + 100)
+    eval_env.seed(seed)
     # Bounding the maximum allowed trajectory length in the environment
     if max_episode_step:
         eval_env._max_episode_steps = max_episode_step
@@ -157,56 +158,67 @@ def policy_interact_with_environment(attack_path, env, state_dim, action_dim, ma
     setting = f"{args.env}_{args.seed}_{args.bcq_max_timesteps}"
     buffer_name = f"target_{args.buffer_name}_{setting}"
 
+    train_states = np.load(f"{attack_path}/buffers/{args.buffer_name}_{args.env}_{args.seed}_state.npy")
+    traj_end_index = np.load(f"{attack_path}/buffers/{args.buffer_name}_{args.env}_{args.seed}_trajectory_end_index.npy")
+    train_initial_states = [train_states[0, :] if i == -1 else train_states[traj_end_index[i]+1, :]
+                      for i in range(-1, len(traj_end_index) - 1)]
+
     # Initialize and load policy
     policy = BCQ.BCQ(state_dim, action_dim, max_action, device, args.discount, args.tau, args.lmbda, args.phi)
     policy.load(f"{attack_path}/models/target_{setting}")
 
     # Initialize buffer
-    replay_buffer = BCQutils.ReplayBuffer(state_dim, action_dim, device, max_size=args.bcq_max_timesteps)
+    replay_buffer = BCQutils.ReplayBuffer(state_dim, action_dim, device, max_size=len(train_initial_states))
     evaluations = []
 
-    state, done = env.reset(), False
-    replay_buffer.initial_state.append(state)
-    episode_reward = 0
-    episode_timesteps = 0
-    episode_num = 0
+    # Env initialization
+    env = gym.make(args.env)
 
+    env.seed(args.seed)
+    # Bounding the maximum allowed trajectory length in the environment
+    env._max_episode_steps = args.max_traj_len
+    torch.manual_seed(args.seed)
+    np.random.seed(args.seed)
+    # state = env.reset()
+
+    episode_num = 0
+    total_t = 0
     # Interact with the environment for max_timesteps
-    for t in range(int(args.bcq_max_timesteps)):
-        episode_timesteps += 1
+    for i in range(len(train_initial_states)):
+        state, done = env.reset(), False
+        if not np.array_equal(state, train_initial_states[i].ravel()):
+            raise ValueError('The initial state is not the same as that in the training data')
+        replay_buffer.initial_state.append(state)
+        episode_reward = 0
+        episode_timesteps = 0
+        episode_num += 1
         # Select action using the target policy
-        action = (
-                policy.select_action(np.array(state))
-                + np.random.normal(0, max_action * args.gaussian_std, size=action_dim)
-        ).clip(-max_action, max_action)
+        while not done:
+            episode_timesteps += 1
+            total_t += 1
+            action = policy.select_action(np.array(state)).clip(-max_action, max_action)
 
         # Perform action
-        next_state, reward, done, _ = env.step(action)
+            next_state, reward, done, _ = env.step(action)
         # TODO: check if we need this line. This is because, we set max_episode step when we instantiate the env. Susan: No need to double check "done". The max_episode step is taking care of it.
         # Then, env should know we have reached that state and return done=True. In fact the code in gym, seems to be doing that.
         # done_bool = float(done) if episode_timesteps < env._max_episode_steps else 0
 
         # Store data in replay buffer
-        replay_buffer.add(state, action, next_state, reward, float(done))
+            replay_buffer.add(state, action, next_state, reward, float(done))
 
-        state = next_state
-        episode_reward += reward
-
-        if done:
+            state = next_state
+            episode_reward += reward
             # +1 to account for 0 indexing. +0 on ep_timesteps since it will increment +1 even if done=True
-            print(
-                f"Total T: {t + 1} Episode Num: {episode_num + 1} Episode T: {episode_timesteps} Reward: {episode_reward:.3f}")
-            # Reset environment
-            state, done = env.reset(), False
-            replay_buffer.initial_state.append(state)
-            episode_reward = 0
-            episode_timesteps = 0
-            episode_num += 1
+        print(
+             f"Total T: {total_t + 1}, Episode Num: {episode_num}, Episode T: {episode_timesteps}, "
+             f"Reward: {episode_reward:.3f}")
+
 
     # Save final buffer and performance
     evaluations.append(eval_policy(policy, args.env, args.seed, max_episode_step=args.max_traj_len))
     np.save(f"{attack_path}/results/target_buffer_performance_{setting}", evaluations)
-    replay_buffer.save(f"{attack_path}/buffers/{buffer_name}")
+    replay_buffer.save(f"{attack_path}/buffers/{buffer_name}_compatible")
 
 
 if __name__ == "__main__":
